779613dd947249b62c30b043573e6e34ccb5b614
BryanD-code <davidbgmartinez@gmail.com>
2026-01-20 12:23:32 +0100

    Resubiendo proyecto a nuevo repositorio

--- firebase-config.js
+++ firebase-config.js
@@ -4,12 +4,13 @@
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-export const firebaseConfig = {
-  apiKey: "AIzaSyBSRGWtiJwXNiNQlW3OZhGiT6J2NLcjdk8",
-  authDomain: "entreno-app.firebaseapp.com",
-  projectId: "entreno-app",
-  storageBucket: "entreno-app.firebasestorage.app",
-  messagingSenderId: "87893890720",
-  appId: "1:87893890720:web:0ab1cfdcc76891a104dbc5"
-};
+require("dotenv").config(); // para cargar variables de entorno desde un archivo .env
 
+export const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+};
--- src/features/entrenador/screens/EntrenadorScreen.jsx
+++ src/features/entrenador/screens/EntrenadorScreen.jsx
@@ -1,14 +1,35 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import HomeScreen from '../../inicio/screens/HomeScreen'
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import HomeScreen from '../../inicio/screens/HomeScreen';
 
 const EntrenadorScreen = () => {
+
+  
+  const entrenamientoEjemplo = {
+    id: "1",
+    nombre: "RUTINA MOVILIDAD",
+    grupo: "Movilidad",
+    instrucciones: "Hombros, cuello y tronco + general",
+    videoUrl: null
+  };
+
   return (
-    <View>
+    <View style={styles.container}>
        <HomeScreen />
-      <Text>EntrenadorScreen</Text>
+      
+       <View style={styles.card}>
+          <Text style={styles.titulo}>{entrenamientoEjemplo.nombre}</Text>
+          <Text>Grupo: {entrenamientoEjemplo.grupo}</Text>
+          <Text>Instrucciones: {entrenamientoEjemplo.instrucciones}</Text>
+       </View>
     </View>
-  )
+  );
 }
 
-export default EntrenadorScreen
+const styles = StyleSheet.create({
+  container: { flex: 1 },
+  card: { padding: 20, margin: 10, backgroundColor: '#eee', borderRadius: 8 },
+  titulo: { fontWeight: 'bold', fontSize: 18, marginBottom: 5 }
+});
+
+export default EntrenadorScreen;
